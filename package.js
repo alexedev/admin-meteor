079f8e843a00ec8f9477160ee45efa40f77c86ba
buduguru <work@alex@gmail.com>
2015-02-21 22:35:48 +0300

    removed demo boxes, updated package version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "buduguru:admin",
   summary: "A complete admin dashboard solution",
-  version: "1.1.1",
+  version: "1.1.2",
   git: "https://github.com/buduguru/meteor-admin"
 });
 

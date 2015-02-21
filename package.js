d094f80e7b9eb202a24f565dcc34f2a30c8b4e84
buduguru <work@alex@gmail.com>
2015-02-21 22:59:46 +0300

    updated version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "buduguru:admin",
   summary: "A complete admin dashboard solution",
-  version: "1.1.3",
+  version: "1.1.4",
   git: "https://github.com/buduguru/meteor-admin-stable"
 });
 
